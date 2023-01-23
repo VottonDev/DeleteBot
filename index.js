b47da2cb288df98504d1a9a9b3d1cc0124b469a6
Otto Hooper <votton.hooper@gmail.com>
2023-01-23 22:55:44 +0000

    Add missing modules

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 require('dotenv').config();
 const { Client, Collection, Events, GatewayIntentBits } = require('discord.js');
+const fs = require('node:fs');
+const path = require('node:path');
 
 const DiscordClient = new Client({
   intents: GatewayIntentBits.Guilds,
