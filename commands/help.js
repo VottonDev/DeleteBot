0e1bdaafa540e42e2428dbf2a6b837b19ecb339b
Otto Hooper <votton.hooper@gmail.com>
2023-01-23 22:40:31 +0000

    Add help command

---
+++ commands/help.js
@@ -0,0 +1,11 @@
+const { SlashCommandBuilder } = require('discord.js');
+
+// Add help command
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('help')
+        .setDescription('Link to support server'),
+    async execute(interaction) {
+        await interaction.reply('https://discord.gg/BgqHYkG');
+    },
+};
