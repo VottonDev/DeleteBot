e1e2030f281c2b1bbec7e0971c7ba7539a9cfee6
Otto Hooper <votton.hooper@gmail.com>
2023-01-24 02:30:17 +0000

    Fix ping logic

--- commands/ping.js
+++ commands/ping.js
@@ -8,9 +8,9 @@ module.exports = {
     // Get the time when the command was executed
     const startTime = Date.now();
     // Get the time when the message was sent
-    const endTime = Date.now();
+    const endTime = interaction.createdTimestamp;
     // Calculate the time it took to send the message
-    const time = endTime - startTime;
+    const time = Math.abs(endTime - startTime);
     // Create an embed
     const embed = new EmbedBuilder().setTitle('Ping').setDescription(`Time to send message: ${time}ms`).setColor(0x00ff00);
     // Edit the message with the embed
