ecda364609fb175becad2c8e478d9e98f8d752f2
Otto Hooper <votton.hooper@gmail.com>
2023-01-24 12:46:21 +0000

    Command to list all the required permissions of the bot

---
+++ commands/listrequiredpermissions.js
@@ -0,0 +1,16 @@
+const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
+
+// Add a command to list the required permissions to make the bot function
+module.exports = {
+  data: new SlashCommandBuilder().setName('listrequiredpermissions').setDescription('List the required permissions to make the bot function').setDMPermission(true),
+  async execute(interaction) {
+    // Create an embed
+    const embed = new EmbedBuilder()
+      .setTitle('Required Permissions')
+      .setDescription('The bot requires the following permissions to function:\n\n- Manage Channels\n- Manage Messages\n- Send Messages\n- Read Message History')
+      .setColor(0x00ff00);
+    // Send the embed
+    await interaction.deferReply();
+    await interaction.editReply({ embeds: [embed], ephemeral: true });
+  },
+};
